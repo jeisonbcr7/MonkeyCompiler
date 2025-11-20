9f11e005a402efe170fca02e8da63f3876042041
jeisonbcr7 <113469434+jeisonbcr7@users.noreply.github.com>
2025-11-19 18:04:01 -0600

    Add console project with ANTLR generation

---
+++ src/MonkeyCompiler/Program.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MonkeyCompiler;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, Monkey compiler!");
+    }
+}
